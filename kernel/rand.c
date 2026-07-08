26fcc9820d3f0f0f082668869fefebb45f6b1a06
mreza127 <m.reza.576084@gmail.com>
2026-07-08 20:07:23 +0330

    add rand.c

---
+++ kernel/rand.c
@@ -0,0 +1,11 @@
+#include "types.h"
+#include "defs.h"
+
+static unsigned int rand_state = 1;
+
+int
+my_rand(void)
+{
+  rand_state = rand_state * 1103515245 + 12345 + ticks;
+  return (int)((rand_state >> 16) & 0x7fff);
+}
